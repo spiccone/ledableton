b99734b35537297cb545ff777a2e9d9e40ac638b
Samantha Piccone <piccones@gmail.com>
2022-11-24 12:03:49 -0700

    Change web-interface to svelte

---
+++ web-interface/src/components/DevicePreview.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type {Device} from "../types";
+  import DeviceColorPicker from './DeviceColorPicker.svelte';
+
+  export let device: Device;
+</script>
+
+<div class="DevicePreview">
+    <div class="device-toolbar">
+        <DeviceColorPicker bind:color={device.color} />
+        <div class="device-label">{device.label}</div>
+    </div>
+</div>
+
+<style>
+  .DevicePreview {
+    box-sizing: border-box;
+    flex: 100 0 auto; /* grow is large to make AddDevice small */
+  }
+  .device-toolbar {
+    align-content: center;
+    box-sizing: border-box;
+    display: flex;
+    padding: 8px;
+  }
+  .device-label {
+    font-size: 14px;
+    flex: 1 1 auto;
+  }
+</style>
---
+++ web-interface/src/components/TimelineList.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import {Device, LayoutDirection as Layout} from "../types";
+	import DeviceTimeline from './DeviceTimeline.svelte';
+
+  export let layoutDirection: Layout = Layout.column;
+  export let devices: Array<Device> = [];
+</script>
+
+<div class="TimelineList {layoutDirection == Layout.column ? 'layout-column' : 'layout-row'}">
+  {#each devices as device, i (device.previewElementId)}
+    <DeviceTimeline device={device} layoutDirection={layoutDirection} />
+  {/each}
+</div>
+
+<style>
+  .TimelineList {
+    display: flex;
+    flex: 1 1 auto; 
+    height: 100%;
+    width: 100%;
+  }
+  .layout-column {
+    flex-direction: column;
+    overflow-y: scroll;
+  }
+  .layout-row {
+    flex-direction: row;
+    overflow-x: scroll;
+  }
+</style>
