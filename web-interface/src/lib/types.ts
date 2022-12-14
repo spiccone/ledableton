ecf30ccf0114df5eaf4da60f5a92658922259ef5
Samantha Piccone <piccones@gmail.com>
2022-12-13 19:05:06 -0700

    Tried to change field value, doesn't work with binding

--- web-interface/src/lib/types.ts
+++ web-interface/src/lib/types.ts
@@ -60,47 +60,154 @@ export class Field {
 
 export class DeviceFieldValue {
   key: string;
-  value: number;
-  repeatedValue: number[];
-  nestedRepeatedValue: number[][];
+  value: number | (number | number[])[];
   unitKey: number | null;
   oneofKey: number;
-  booleanValue : boolean;
+  boolean = false;
 
   constructor(key: string, unitKey: number | null) {
     this.key = key;
     this.value = 0;
     this.unitKey = unitKey;
     this.oneofKey = 0;
-    this.repeatedValue = [];
-    this.nestedRepeatedValue = [];
-    this.booleanValue = false;
+  } 
+
+  getValueLength(index?: number) {
+    if(Array.isArray(this.value)) {
+      if (index === undefined) {
+        return this.value.length;
+      } else if (index < this.value.length) {
+        const nested = this.value[index];
+        if (Array.isArray(nested)) {
+          return nested.length;
+        }
+      }
+    }
+    return -1;
   }
 
-  addToRepeatedValue(value: number[] | number) {
-    if (typeof value === "number") {
-      this.repeatedValue.push(value);
-    } else {
-      this.nestedRepeatedValue.push(value);
+  getValue(i?: number, j?: number) {
+    if (i === undefined) {
+      return this.value;
     }
+    if (Array.isArray(this.value)) {
+      if (i >= this.value.length) {
+        return null;
+      }
+      if (j === undefined) {
+        return this.value[i];
+      }
+      const nested = this.value[i];
+      if (Array.isArray(nested) && j < nested.length) {
+        return j;
+      }
+    }
+    return null;
   }
 
-  removeFromRepeatedValue() {
-    this.repeatedValue.pop();
-    this.nestedRepeatedValue.pop();
+  getValueAsArray(i?: number) {
+    if (!Array.isArray(this.value)) {
+      return [this.value]
+    }
+    if (i === undefined) {
+      return this.value;
+    }
+    if (i < this.value.length) {
+      const nested = this.value[i];
+      if (Array.isArray(nested)) {
+        return nested;
+      }
+      return [nested];
+    }
+    return [];
   }
 
-  addToNestedRepeatedValue(index: number, value: number) {
-    if (index < this.nestedRepeatedValue.length) {
-      this.nestedRepeatedValue[index].push(value);
+  addToValue(value: number|number[], index?: number) {
+    if (!Array.isArray(this.value)) {
+      return false;
     }
+    if (index === undefined) {
+      this.value.push(value);
+      return true;
+    }
+    if (!Array.isArray(value) && 
+      index < this.value.length) {
+        const nested = this.value[index];
+        if (Array.isArray(nested)) {
+          nested.push(value);
+          return true;
+        }
+    }
+    return false;
   }
-  removeFromNestedRepeatedValue(index: number) {
-    if (index < this.nestedRepeatedValue.length) {
-      this.nestedRepeatedValue[index].pop();
+
+  popFromValue(index?: number) {
+    if (!Array.isArray(this.value)) {
+      return null;
     }
+    if (index === undefined) {
+      return this.value.pop();
+    }
+    if (index < this.value.length) {
+      const nested = this.value[index];
+        if (Array.isArray(nested)) {
+          return nested.pop();
+        }
+    }
+    return null;
+  }
+
+  updateValue(value: number|number[], i?: number, j?: number) {
+    if (i === undefined) {
+      this.value = value;
+      return true;
+    }
+    if (Array.isArray(this.value)) {
+      if (i >= this.value.length) {
+        return false;
+      }
+      if (j === undefined) {
+        this.value[i] = value;
+        return true;
+      }
+      if (Array.isArray(value)) {
+        return false;
+      }
+      const nested = this.value[i];
+      if (Array.isArray(nested) && j < nested.length) {
+       nested[j] = value;
+       return true;
+      }
+    }
+    return false;
   }
 }
+
+
+  // addToRepeatedValue(value: number[] | number) {
+  //   if (typeof value === "number") {
+  //     this.repeatedValue.push(value);
+  //   } else {
+  //     this.nestedRepeatedValue.push(value);
+  //   }
+  // }
+
+  // removeFromRepeatedValue() {
+  //   this.repeatedValue.pop();
+  //   this.nestedRepeatedValue.pop();
+  // }
+
+  // addToNestedRepeatedValue(index: number, value: number) {
+  //   if (index < this.nestedRepeatedValue.length) {
+  //     this.nestedRepeatedValue[index].push(value);
+  //   }
+  // }
+  // removeFromNestedRepeatedValue(index: number) {
+  //   if (index < this.nestedRepeatedValue.length) {
+  //     this.nestedRepeatedValue[index].pop();
+  //   }
+  // }
+// }
 
 export class DeviceType {
   key: string;
@@ -119,6 +226,7 @@ export class SavedDevice {
   label: string;
   type: DeviceType | null = null;
   fields: DeviceFieldValue[] | null = null;
+  bucket: SavedDevice | null = null;
   bucketType: DeviceType | null = null;
   bucketFields: DeviceFieldValue[] | null = null;
 
